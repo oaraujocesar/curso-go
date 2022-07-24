89bd7c2060b484c9ef611ba8aa726d467ddc1a4f
César O. Araújo <oaraujocesar@gmail.com>
2022-07-24 17:20:47 -0300

    exploring ways of declaring variables and constants

--- fundamentals/constvar/constvar.go
+++ fundamentals/constvar/constvar.go
@@ -11,5 +11,25 @@ func main() {
 
 	area := PI * m.Pow(ray, 2)
 
-	fmt.Printf("The area calculated is: %.2fm2", area)
+	fmt.Printf("The calculated area is: %.2fm2 \n", area)
+
+	const (
+		a = 1
+		b = 2
+	)
+
+	var (
+		c = 3
+		d = 4
+	)
+
+	fmt.Println(a, b, c, d)
+
+	var e, f bool = true, false
+
+	fmt.Println(e, f)
+
+	g, h, i := 2, false, "Oops!"
+
+	fmt.Println(g, h, i)
 }
