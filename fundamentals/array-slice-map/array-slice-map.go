28e19e55ce3f4e7bb1afd76740c652f0a1bfc1d0
César O. Araújo <oaraujocesar@gmail.com>
2022-08-03 10:32:41 -0300

    using copy

--- fundamentals/array-slice-map/array-slice-map.go
+++ fundamentals/array-slice-map/array-slice-map.go
@@ -63,4 +63,19 @@ func main() {
 	arr2[3] = 12
 	fmt.Println(arr, arr2)
 
+	// COPY
+
+	array1 := [3]int{1, 2, 3}
+	var slice1 []int
+
+	// array1 = append(array1, 4, 5, 6)
+	slice1 = append(slice1, 4, 5, 6)
+	fmt.Println(array1, slice1)
+
+	slice2 := make([]int, 2)
+	fmt.Println(slice2)
+
+	copy(slice2, slice1)
+	fmt.Println(slice2)
+
 }
