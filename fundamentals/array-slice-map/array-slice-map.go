f433725fd54150a79eaa93427ba724634822d9e8
César O. Araújo <oaraujocesar@gmail.com>
2022-08-03 00:22:23 -0300

    slices

--- fundamentals/array-slice-map/array-slice-map.go
+++ fundamentals/array-slice-map/array-slice-map.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// ARRAYS
 	var grades [3]float64
 
 	grades[0], grades[1], grades[2] = 7.8, 4.3, 9.1
@@ -19,5 +20,25 @@ func main() {
 
 	average := total / count
 
-	fmt.Printf("%.2f", average)
+	fmt.Printf("%.2f\n", average)
+
+	numbers := [...]int{1, 2, 3, 4, 5}
+
+	for i, number := range numbers {
+		fmt.Printf("%d) %d\n", i, number)
+	}
+
+	// SLICES
+	s1 := []int{1, 2, 3, 4, 5, 6, 7}
+
+	for _, n := range s1 {
+		fmt.Println(n * 3)
+	}
+
+	a2 := [5]uint{1, 2, 3, 4, 5}
+
+	s2 := a2[1:3]
+
+	fmt.Println(s2)
+
 }
