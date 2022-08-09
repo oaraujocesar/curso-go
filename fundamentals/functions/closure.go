875dd6c992db0b9b36760f3dc265fe6e65b07406
César O. Araújo <oaraujocesar@gmail.com>
2022-08-08 21:04:45 -0300

    exploring closures

---
+++ fundamentals/functions/closure.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func closure() func() {
+	x := 10
+
+	funcao := func() {
+		fmt.Println(x)
+	}
+
+	return funcao
+}
+
+func main() {
+	x := 20
+
+	fmt.Println(x)
+
+	printX := closure()
+
+	printX()
+}
--- fundamentals/functions/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	a, b := 3, 5
-	result := sumUp(a, b)
-
-	print(result)
-}
