7ff6209875cfc5f1284ce6f8fea07969b05d7457
César O. Araújo <oaraujocesar@gmail.com>
2022-08-08 21:15:27 -0300

    recursion(recursion)

--- fundamentals/functions/closure.go
+++ fundamentals/functions/closure.go
@@ -12,12 +12,36 @@ func closure() func() {
 	return funcao
 }
 
+func factorial(n int) (int, error) {
+	switch {
+	case n < 0:
+		return -1, fmt.Errorf("Invalid number. Typed number: %d", n)
+	case n == 0:
+		return 1, nil
+	default:
+		previousFactorial, _ := factorial(n - 1)
+		return n * previousFactorial, nil
+	}
+}
+
 func main() {
 	x := 20
 
-	fmt.Println(x)
+	fmt.Println(x) // 20
 
 	printX := closure()
 
-	printX()
+	printX() // 10
+
+	// Factorial
+
+	result, _ := factorial(5)
+
+	fmt.Println(result)
+
+	_, err := factorial(-4)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
