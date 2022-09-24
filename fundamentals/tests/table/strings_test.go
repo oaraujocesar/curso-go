bfd0c1e3d181f1473b17de79ee66ff6146144702
César O. Araújo <oaraujocesar@gmail.com>
2022-09-24 19:16:00 -0300

    using datasets for a better coverage

---
+++ fundamentals/tests/table/strings_test.go
@@ -0,0 +1,31 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (part: %s) - index: expected (%d) <> found (%d)."
+
+func TestIndex(t *testing.T) {
+	tests := []struct {
+		text     string
+		part     string
+		expected int
+	}{
+		{text: "I'm amazing", part: "amazing", expected: 4},
+		{text: "", part: "", expected: 0},
+		{text: "Ops, I spoke too much", part: "ops", expected: -1},
+		{text: "Leonard", part: "o", expected: 2},
+	}
+
+	for _, test := range tests {
+		t.Logf("Mass: %v", test)
+
+		current := strings.Index(test.text, test.part)
+
+		if current != test.expected {
+			t.Errorf(msgIndex, test.text, test.part, test.expected, current)
+		}
+	}
+}
